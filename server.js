2247653c86be760aa6e5e6199326ff8fd7c6df0b
van <vannb170801@gmail.com>
2022-12-05 18:18:31 +0700

    first commit2

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ mongoose.connect('mongodb+srv://gttm_nhom4:van170801@cluster0.m2spfol.mongodb.ne
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
-// craet router   database
+// craet router  database
 var user = require('./config/connectDB')
 var admin = require('./config/adminDB')
 // css
@@ -21,53 +21,8 @@ app.use('/public', express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-// Router home
-// app.get('/home', (req, res)=>{
-//     res.render('home');
-// });
-
-
-app.get('/registeradmin', (req, res)=>{
-    res.render('registeradmin');
-});
-
-app.get('/loginadminfail', (req, res)=>{
-    res.render('loginadminfail');
-});
-
 app.get('/admin', (req, res)=>{
     res.render('admin');
-});
-app.get('/adminfail', (req, res)=>{
-    res.render('adminfail');
-});
-// creat admin
-app.post('/registeradmin', urlencodedParser, async (req, res)=>{
-    try{
-        var useradmin = req.body.useradmin
-        const ad =  await admin.findOne({
-            useradmin: useradmin 
-        })
-        if(ad){
-            alert('Tài khoản admin này đã tồn tại')
-            res.redirect('adminfail')
-            return console.log('Tài khoản admin này đã tồn tại')
-            
-        }
-        const moiadmin = new admin({
-            useradmin: req.body.useradmin,
-            password: req.body.password
-        })
-
-        const newAdmin = await admin.create(moiadmin)
-        alert('Tạo tài khoản admin thành công')
-        res.redirect('/register')
-        console.log('tạo tài khoản admin thành công')
-    }
-    catch(error) {
-        console.log('lỗi server admin')
-    }
-    
 });
 
 // Login admin
@@ -90,16 +45,12 @@ app.post('/admin', urlencodedParser, async (req, res) => {
     .catch(err=> {
         alert('Tài khoản không đúng')
         console.log('Đăng nhập admin thất bại')
-        res.redirect('/loginadminfail')
     })
 });
 
 // Router register
 app.get('/register', (req, res)=>{
     res.render('register');
-});
-app.get('/register/registerfail', (req, res)=>{
-    res.render('registerfail');
 });
 // creat user
 app.post('/register', urlencodedParser, async (req, res)=>{
@@ -110,7 +61,6 @@ app.post('/register', urlencodedParser, async (req, res)=>{
         })
         if(use){
             alert('Tài khoản này đã tồn tại')
-            res.redirect('register/registerfail')
             return console.log('tài khoản người dùng đã tồn tại')
             
         }
@@ -127,15 +77,9 @@ app.post('/register', urlencodedParser, async (req, res)=>{
     catch(error) {
         console.log('loi server')
     }
-    
 });
-
 
 // Router Login
-app.get('/loginfail', (req, res)=>{
-    res.render('loginfail');
-});
-
 app.get('/', (req, res)=>{
     res.render('login');
 });
@@ -159,11 +103,10 @@ app.post('/', urlencodedParser, async (req, res) => {
     .catch(err=> {
         alert('Tài khoản không đúng')
         console.log('Tài khoản không đúng')
-        res.redirect('/loginfail')
     })
 });
 
-// địa chỉ HEROKU || IP localhost
+//IP localhost
 app.listen( process.env.PORT ||3000, () => {
     console.log('Server Bắt đầu tại http://localhost:3000');
 });
